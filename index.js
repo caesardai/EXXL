bc5878daf3660e7b9c2d11bbfbdbe70ed03bac65
Xavier DeVore <xdevore@haverford.edu>
2023-03-24 10:10:38 -0400

    added semicolons for the vibe

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 
 
@@ -12,4 +12,4 @@ app.get('/', function(req, res) {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
