e31ee45f6758ff3a5112d2fa82c61e504fd35c15
Xavier DeVore <xdevore@haverford.edu>
2023-03-24 11:50:31 -0400

    changed font and added jsdom

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const jsdom = require('jsdom');
 const app = express();
 const port = 3000;
 
+console.log(express);
+
+// using jsdom to get HTML information from index.html
+const html = fs.readFileSync(path.join(__dirname, '/index.html'))
+const htmlDocument = new jsdom.JSDOM(html).window.document;
+
+// using current directory
+app.use(express.static(__dirname));
 
 
 // sendFile will go here
