2ff1b3bfc31d0e97e3b2044d93093d62d0a6862f
Leo Costa <porcupineforever@gmail.com>
2023-03-29 14:09:48 -0400

    cleaned up UI for Users

---
+++ webpage/users.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var jsdom = require('jsdom');
+var app = express();
+
+// using current directory
+app.use(express.static(__dirname));
+
+var html = fs.readFileSync(path.join(__dirname, '/users.html'))
+var window = new jsdom.JSDOM(html).window;
+var document = new jsdom.JSDOM(html).window.document;
+
+/**
+ * TODO: fill
+ */
+async function findUsersAndDisplay() {
+
+    // get HTML content of the card to put user list 
+    var groupListCard = document.getElementById("usersCard");
+
+    // get list of users from database
+    await fetch('http://localhost:3000/findUsers').then(async (response) => {
+        if (!response.ok) {
+            const message = `Error: ${response.status}`;
+            throw new Error(message);
+        }
+        else {
+            // creating HTML to contain the users
+            var newListGroupHTML = "";
+            var empty = true;
+
+            await response.json().then((data) => {
+                data.forEach((user) => {
+                    empty = false;
+
+                    // Create box with name and username
+                    newListGroupHTML += '<div class="shadow-none p-3 mb-2 bg-light rounded">';
+                    newListGroupHTML += "<b>" + user.firstName + " " + user.lastName + "</b> (@" + user.username + ") ";
+
+                    // Add delete button
+                    newListGroupHTML += "<a href=\"/deleteUser?username=" + user.username + "\">[Delete]</a><br>";
+
+                    newListGroupHTML += "</div>";
+                });
+            });
+
+            if (empty)
+                newListGroupHTML += "No users in database.";
+
+            // adding HTML we've made here to hotspots.html
+            var newElement = document.createElement("div");
+            newElement.innerHTML = newListGroupHTML;
+            groupListCard.appendChild(newElement);  
+        }
+    });
+}
