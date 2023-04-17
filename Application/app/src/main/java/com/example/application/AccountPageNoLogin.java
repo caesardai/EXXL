31537026eca304d2899d63a7365c5c25c129ec69
Leo Costa <porcupineforever@gmail.com>
2023-04-16 21:30:13 -0400

    adding button functionality and onCreates for account management/creation

--- Application/app/src/main/java/com/example/application/AccountPageNoLogin.java
+++ Application/app/src/main/java/com/example/application/AccountPageNoLogin.java
@@ -1,4 +1,27 @@
 package com.example.application;
 
-public class AccountPageNoLogin {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountPageNoLogin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_page_no_login);
+    }
+
+    public void onLoginButtonClick(View view) {
+        System.out.println("TODO: go to login page");
+    }
+
+    public void onNewAccountButtonClick(View view) {
+        System.out.println("onNewAccountButtonClick");
+        Intent in = new Intent(this, CreateAccount.class);
+        in.putExtra("Message", "random message; new account time");
+        startActivityForResult(in, 1);
+    }
 }
--- Application/app/src/main/java/com/example/application/CreateAccount.java
+++ Application/app/src/main/java/com/example/application/CreateAccount.java
@@ -1,2 +1,23 @@
-package com.example.application;public class CreateAccount {
+package com.example.application;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccount  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+    }
+
+    public void onCreateButtonClick(View view) {
+        String first_name = ((EditText) findViewById(R.id.first_name)).getText().toString().trim();
+        String last_name = ((EditText) findViewById(R.id.last_name)).getText().toString().trim();
+        String username = ((EditText) findViewById(R.id.username)).getText().toString().trim();
+        String password = ((EditText) findViewById(R.id.password)).getText().toString().trim();
+    }
 }
