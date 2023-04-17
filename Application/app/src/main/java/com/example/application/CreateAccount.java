bb087a50562c8d1ff6d7f1b3f8712b17c7509157
Leo Costa <porcupineforever@gmail.com>
2023-04-17 15:44:04 -0400

    create_account.xml; onCreateAccountButtonBlick()

--- Application/app/src/main/java/com/example/application/CreateAccount.java
+++ Application/app/src/main/java/com/example/application/CreateAccount.java
@@ -6,7 +6,7 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CreateAccount  extends AppCompatActivity {
+public class CreateAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +14,31 @@ public class CreateAccount  extends AppCompatActivity {
         setContentView(R.layout.create_account);
     }
 
-    public void onCreateButtonClick(View view) {
+    public void onCreateAccountButtonClick(View view) {
+        boolean validInput = true;
+        System.out.println("onCreateAccountButtonClick pressed");
         String first_name = ((EditText) findViewById(R.id.first_name)).getText().toString().trim();
+        System.out.println(first_name); // look under Run for print statements
         String last_name = ((EditText) findViewById(R.id.last_name)).getText().toString().trim();
+
         String username = ((EditText) findViewById(R.id.username)).getText().toString().trim();
-        String password = ((EditText) findViewById(R.id.password)).getText().toString().trim();
+
+        String password = ((EditText) findViewById(R.id.password)).getText().toString();
+        String repassword = ((EditText) findViewById(R.id.repassword)).getText().toString();
+
+        if (password.isEmpty() || repassword.isEmpty()) {
+            // invalid password Toast
+            return;
+        }
+
+        if (password.equals(repassword)) {
+            // invalid password Toast
+            return;
+        }
+
+        if (validInput) {
+            // add to database
+            // exit out of registration page
+        }
     }
 }
